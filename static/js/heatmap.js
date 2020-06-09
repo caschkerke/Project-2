ed5bdd9f4760dae1f04c9ddb461d1761c9073031
caschkerke <caschkerke@gmail.com>
2020-06-09 00:00:21 -0500

    Revert "updating branch"
    This reverts commit 5eb7ccc0a2b7b05f4fd5a20282c87ed9c7092d68.

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -1,10 +1,8 @@
-// The code below is functioning as a placeholder and will serve as a reference for the new heatmap code in the future.
 var myMap = L.map("map", {
     center: [TODO],
     zoom: TODO
 });
 
-// Ensure you've created a config.js file with a working mapbox API key before proceeding.
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
