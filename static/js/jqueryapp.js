f69fc39aa5b26328823e17e9bd2b3e08d46ef97c
caschkerke <caschkerke@gmail.com>
2020-06-09 01:59:59 -0500

    data update
    updating and merging datasets again since file can be hosted in repo with new git LFS settings. minor updates/tests on other files.

--- static/js/jqueryapp.js
+++ static/js/jqueryapp.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {   
-// Assigning dataset variables.
-    var dataFirst = $.getJSON("database_2016.json");
-    var dataSecond = $.getJSON("database_2017.json");
-    var dataThird = $.getJSON("database_2018.json");
-    var dataFourth = $.getJSON("database_2019.json");
-// Selecting dropdown menus.
-    var weatherMenu = $("#selWeather");
-    var envMenu = $("#selEnvironment");
-// Creating empty lists to hold weather and environment values.
-    var weatherAll = [];
-    var envAll = [];
-// Using onlyunique function to filter unique values.
-    var weatherUniq = weatherAll.filter(onlyUnique);
-    var envUniq = envAll.filter(onlyUnique);
+// // Assigning dataset variables.
+//     var dataFirst = $.getJSON("database_2016.json");
+//     var dataSecond = $.getJSON("database_2017.json");
+//     var dataThird = $.getJSON("database_2018.json");
+//     var dataFourth = $.getJSON("database_2019.json");
+// // Selecting dropdown menus.
+//     var weatherMenu = $("#selWeather");
+//     var envMenu = $("#selEnvironment");
+// // Creating empty lists to hold weather and environment values.
+//     var weatherAll = [];
+//     var envAll = [];
+// // Using onlyunique function to filter unique values.
+//     var weatherUniq = weatherAll.filter(onlyUnique);
+//     var envUniq = envAll.filter(onlyUnique);
 
-// Pulling every weather and environment entry from each year.
-    for (var i = 0; i < dataFirst.length; i++){
-        var weather = dataFirst[i].weather;
-        var env = dataFirst[i].environment;
-        weatherAll.push(weather);
-        envAll.push(env);
-    }
-    for (var i = 0; i < dataSecond.length; i++){
-        var weather = dataSecond[i].weather;
-        var env = dataSecond[i].environment;
-        weatherAll.push(weather);
-        envAll.push(env);
-    }
-    for (var i = 0; i < dataThird.length; i++){
-        var weather = dataThird[i].weather;
-        var env = dataThird[i].environment;
-        weatherAll.push(weather);
-        envAll.push(env);
-    }
-    for (var i = 0; i < dataFourth.length; i++){
-        var weather = dataFourth[i].weather;
-        var env = dataFourth[i].environment;
-        weatherAll.push(weather);
-        envAll.push(env);
-    }
+// // Pulling every weather and environment entry from each year.
+//     for (var i = 0; i < dataFirst.length; i++){
+//         var weather = dataFirst[i].weather;
+//         var env = dataFirst[i].environment;
+//         weatherAll.push(weather);
+//         envAll.push(env);
+//     }
+//     for (var i = 0; i < dataSecond.length; i++){
+//         var weather = dataSecond[i].weather;
+//         var env = dataSecond[i].environment;
+//         weatherAll.push(weather);
+//         envAll.push(env);
+//     }
+//     for (var i = 0; i < dataThird.length; i++){
+//         var weather = dataThird[i].weather;
+//         var env = dataThird[i].environment;
+//         weatherAll.push(weather);
+//         envAll.push(env);
+//     }
+//     for (var i = 0; i < dataFourth.length; i++){
+//         var weather = dataFourth[i].weather;
+//         var env = dataFourth[i].environment;
+//         weatherAll.push(weather);
+//         envAll.push(env);
+//     }
 
-// Creating option lists using only the unique values.
-    for (var i = 0; i < weatherUniq.length; i++){
-        var ele = document.createElement("option");
-        ele.textContent = weatherUniq[i];
-        ele.value = weatherUniq[i];
-        weatherMenu.appendChild(ele);
-    }
-    for (var i = 0; i < envUniq.length; i++){
-        var ele = document.createElement("option");
-        ele.textContent = envUniq[i];
-        ele.value = envUniq[i];
-        envMenu.appendChild(ele);
-    };
+// // Creating option lists using only the unique values.
+//     for (var i = 0; i < weatherUniq.length; i++){
+//         var ele = document.createElement("option");
+//         ele.textContent = weatherUniq[i];
+//         ele.value = weatherUniq[i];
+//         weatherMenu.appendChild(ele);
+//     }
+//     for (var i = 0; i < envUniq.length; i++){
+//         var ele = document.createElement("option");
+//         ele.textContent = envUniq[i];
+//         ele.value = envUniq[i];
+//         envMenu.appendChild(ele);
+//     };
 
     var usMap = L.map("countryMap", {
         center: [31.51073, -96.4247],
@@ -66,9 +66,9 @@ $(document).ready(function() {
         accessToken: API_KEY
     }).addTo(usMap);
 
-    $("#selYear").change(updatePlotly())
-    $("#selWeather").change(updatePlotly())
-    $("#selEnvironment").change(updatePlotly())
+    // $("#selYear").change(updatePlotly())
+    // $("#selWeather").change(updatePlotly())
+    // $("#selEnvironment").change(updatePlotly())
 
 });
 
