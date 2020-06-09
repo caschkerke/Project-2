d41b33526317701da542d083e8c83864949d966c
caschkerke <caschkerke@gmail.com>
2020-06-09 05:34:52 -0500

    updating progress
    Updating jquery app file, tentative flask app, and index.html. Removing old app.js file as it is now obsolete.

--- static/js/jqueryapp.js
+++ static/js/jqueryapp.js
@@ -1,58 +1,65 @@
 $(document).ready(function() {   
-// // Assigning dataset variables.
-//     var dataFirst = $.getJSON("database_2016.json");
-//     var dataSecond = $.getJSON("database_2017.json");
-//     var dataThird = $.getJSON("database_2018.json");
-//     var dataFourth = $.getJSON("database_2019.json");
-// // Selecting dropdown menus.
-//     var weatherMenu = $("#selWeather");
-//     var envMenu = $("#selEnvironment");
-// // Creating empty lists to hold weather and environment values.
-//     var weatherAll = [];
-//     var envAll = [];
-// // Using onlyunique function to filter unique values.
-//     var weatherUniq = weatherAll.filter(onlyUnique);
-//     var envUniq = envAll.filter(onlyUnique);
+// Selecting dropdown menus.
+    var weatherMenu = $("#selWeather");
+    var envMenu = $("#selEnvironment");
+    var yearMenu = $("#selYear");
+// Due to the nature of the visualizations relating to the environments, using a static list of values.
+    var envUniq = ["Bump",
+                   "Crossing",
+                   "Give_Way",
+                   "Junction",
+                   "No_Exit",
+                   "Railway",
+                   "Roundabout",
+                   "Station",
+                   "Stop",
+                   "Traffic_Calming",
+                   "Traffic_Signal",
+                   "Turning_Loop"];
 
-// // Pulling every weather and environment entry from each year.
-//     for (var i = 0; i < dataFirst.length; i++){
-//         var weather = dataFirst[i].weather;
-//         var env = dataFirst[i].environment;
-//         weatherAll.push(weather);
-//         envAll.push(env);
-//     }
-//     for (var i = 0; i < dataSecond.length; i++){
-//         var weather = dataSecond[i].weather;
-//         var env = dataSecond[i].environment;
-//         weatherAll.push(weather);
-//         envAll.push(env);
-//     }
-//     for (var i = 0; i < dataThird.length; i++){
-//         var weather = dataThird[i].weather;
-//         var env = dataThird[i].environment;
-//         weatherAll.push(weather);
-//         envAll.push(env);
-//     }
-//     for (var i = 0; i < dataFourth.length; i++){
-//         var weather = dataFourth[i].weather;
-//         var env = dataFourth[i].environment;
-//         weatherAll.push(weather);
-//         envAll.push(env);
-//     }
+    $('#selYear').change(function(){
+        var year = this.value;
+        alert(year);
+        if (year == "2016") {
 
-// // Creating option lists using only the unique values.
-//     for (var i = 0; i < weatherUniq.length; i++){
-//         var ele = document.createElement("option");
-//         ele.textContent = weatherUniq[i];
-//         ele.value = weatherUniq[i];
-//         weatherMenu.appendChild(ele);
-//     }
-//     for (var i = 0; i < envUniq.length; i++){
-//         var ele = document.createElement("option");
-//         ele.textContent = envUniq[i];
-//         ele.value = envUniq[i];
-//         envMenu.appendChild(ele);
-//     };
+        } else if (year == "2017") {
+
+        } else if (year == "2018") {
+
+        } else if (year == "2019") {
+
+        } else {
+
+        }
+    })
+
+    $('#selWeather').change(function(){
+
+    })
+
+    $('#selEnvironment').change(function(){
+
+    })
+// Function that can be used to filter arrays by unique values.
+    function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+    }
+// Using onlyunique function to filter unique values.
+    var weatherUniq = $.getJSON("api/weather").filter(onlyUnique);
+
+// Creating option lists using only the unique values.
+    for (var i = 0; i < weatherUniq.length; i++){
+        var ele = document.createElement("option");
+        ele.textContent = weatherUniq[i];
+        ele.value = weatherUniq[i];
+        weatherMenu.appendChild(ele);
+    }
+    for (var i = 0; i < envUniq.length; i++){
+        var ele = document.createElement("option");
+        ele.textContent = envUniq[i];
+        ele.value = envUniq[i];
+        envMenu.appendChild(ele);
+    };
 
     var usMap = L.map("countryMap", {
         center: [31.51073, -96.4247],
@@ -66,33 +73,4 @@ $(document).ready(function() {
         accessToken: API_KEY
     }).addTo(usMap);
 
-    // $("#selYear").change(updatePlotly())
-    // $("#selWeather").change(updatePlotly())
-    // $("#selEnvironment").change(updatePlotly())
-
 });
-
-// Function that can be used to filter arrays by unique values.
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
-
-function updatePlotly() {
-    
-    var dataFirst = $.getJSON("database_2016.json");
-    var dataSecond = $.getJSON("database_2017.json");
-    var dataThird = $.getJSON("database_2018.json");
-    var dataFourth = $.getJSON("database_2019.json");
-    var selectedYear = $("#selYear").val();
-    var selectedWeather = $("#selWeather").val();
-    var selectedEnv = $("#selEnvironment").val();
-
-    console.log(dataFirst)
-
-    // ---- If needed ----
-    // var emptylist = [];
-    // var emptylist2 = [];
-    // var emptylistEtc = [];
-
-    // for (var i = 0; i < 1; i++);
-}
