b4441f6e65ab114cdb95529e2aacdbe780bddcbb
caschkerke <caschkerke@gmail.com>
2020-06-09 00:02:38 -0500

    Revert "updating branch"
    This reverts commit 5eb7ccc0a2b7b05f4fd5a20282c87ed9c7092d68.

--- static/app.py
+++
@@ -1,7 +0,0 @@
-# ----- Flask App Code Here -----
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
-
