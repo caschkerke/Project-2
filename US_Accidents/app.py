da3fce9ee1877219dd2fd948467e43997234bda6
caschkerke <caschkerke@gmail.com>
2020-06-09 11:15:29 -0500

    Updating
    Further updates to javascript app and of course more restructuring. Flask app should already be finished? Models for app should also be completed.

--- US_Accidents/app.py
+++ US_Accidents/app.py
@@ -9,6 +9,9 @@ from flask import (
     redirect)
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
+from .models import (
+    db2016, db2017, db2018, db2019
+    )
 
 #################################################
 # Flask Setup
@@ -22,15 +25,11 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///all_data.sqlite"
 
-# # Remove tracking modifications
+# Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from .models import ( db2016, 
-                      db2017, 
-                      db2018, 
-                      db2019 )
 
 # from sqlalchemy.ext.declarative import declarative_base
 # from sqlalchemy.orm import Session
@@ -49,32 +48,37 @@ def home():
 
 @app.route("/api/weather")
 def weather():
-    results = db.session.query(db_2016.Weather_Condition, 
-                               db_2017.Weather_Condition, 
-                               db_2018.Weather_Condition, 
-                               db_2019.Weather_Condition).all()
+    results = db.session.query(db2016.Weather_Condition, 
+                               db2017.Weather_Condition, 
+                               db2018.Weather_Condition, 
+                               db2019.Weather_Condition).all()
     
     return jsonify(results)
 
-@app.route("/api/environment")
-def environment():
+@app.route("/api/data2016")
+def data2016():
+    results = db2016.query.all()
 
-    environments = db.session.query(func.count)
-        
-    return jsonify(environments)
+    return jsonify(results)
 
-@app.route("/api/db2016")
-def db2016():
-    results = db.sess
+@app.route("/api/data2017")
+def data2017():
+    results = db2017.query.all()
 
-@app.route("/api/db2017")
-def db2017():
-    
+    return jsonify(results)
 
-@app.route("/api/db2018")
-def db2018():
-    
+@app.route("/api/data2018")
+def data2018():
+    results = db2018.query.all()
 
-@app.route("/api/db2019")
-def db2019():
+    return jsonify(results)
+
+@app.route("/api/data2019")
+def data2019():
+    results = db2019.query.all()
+
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run()
     
--- US_Accidents/static/js/app.js
+++ US_Accidents/static/js/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {   
+
 // Selecting dropdown menus.
     var weatherMenu = $("#selWeather");
     var envMenu = $("#selEnvironment");
-    var yearMenu = $("#selYear");
-// Due to the nature of the visualizations relating to the environments, using a static list of values.
+
+// Function that can be used to filter arrays by unique values.
+    function onlyUnique(value, index, self) { 
+        return self.indexOf(value) === index;
+    }
+
+// Due to the nature of the visualizations relating to the environments, and the fact that the different environments are individual columns, using a static list.
     var envUniq = ["Bump",
                    "Crossing",
                    "Give_Way",
@@ -17,49 +23,89 @@ $(document).ready(function() {
                    "Traffic_Signal",
                    "Turning_Loop"];
 
-    $('#selYear').change(function(){
-        var year = this.value;
-        alert(year);
-        if (year == "2016") {
-
-        } else if (year == "2017") {
-
-        } else if (year == "2018") {
-
-        } else if (year == "2019") {
-
-        } else {
-
-        }
-    })
-
-    $('#selWeather').change(function(){
-
-    })
-
-    $('#selEnvironment').change(function(){
-
-    })
-// Function that can be used to filter arrays by unique values.
-    function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-    }
 // Using onlyunique function to filter unique values.
-    var weatherUniq = $.getJSON("api/weather").filter(onlyUnique);
-
-// Creating option lists using only the unique values.
+    var weatherUniq = $.getJSON("/api/weather").filter(onlyUnique);
+    
+// Populating dropdown menus with values.
     for (var i = 0; i < weatherUniq.length; i++){
         var ele = document.createElement("option");
         ele.textContent = weatherUniq[i];
         ele.value = weatherUniq[i];
         weatherMenu.appendChild(ele);
-    }
+    };
+
     for (var i = 0; i < envUniq.length; i++){
         var ele = document.createElement("option");
         ele.textContent = envUniq[i];
         ele.value = envUniq[i];
         envMenu.appendChild(ele);
     };
+
+    var menuChange = function(){
+        
+        var year = $('#selYear').value;
+        var weather = $('#selWeather').value;
+        var env = $('#selEnvironment').value;
+
+        if (year == "2016") {
+            var data = $.getJSON("/api/data2016");
+            if ( weather == ) {
+
+            } else {
+
+            }
+            if ( env == ) {
+
+            } else {
+
+            }
+        } else if (year == "2017") {
+            var data = $.getJSON("/api/data2017");
+            if ( weather == ) {
+
+            } else {
+
+            }
+            if ( env == ) {
+
+            } else {
+                
+            }
+        } else if (year == "2018") {
+            var data = $.getJSON("/api/data2018");
+            if ( weather == ) {
+
+            } else {
+
+            }
+            if ( env == ) {
+
+            } else {
+                
+            }
+        } else if (year == "2019") {
+            var data = $.getJSON("/api/data2019");
+            if ( weather == ) {
+
+            } else {
+
+            }
+            if ( env == ) {
+
+            } else {
+                
+            }
+        } else {
+        
+        }
+        
+    }
+
+    $('#selYear').change(menuChange);
+    $('#selWeather').change(menuChange);
+    $('#selEnvironment').change(menuChange);
+
+// ---- Visualization creation begins here ----
 
     var usMap = L.map("countryMap", {
         center: [31.51073, -96.4247],
