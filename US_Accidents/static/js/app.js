2dbad31fcb6c7f3d560553fcab823b9a6f8e8e34
caschkerke <caschkerke@gmail.com>
2020-06-09 19:19:28 -0500

    Update app.js

--- US_Accidents/static/js/app.js
+++ US_Accidents/static/js/app.js
@@ -108,28 +108,28 @@ $(document).ready(function() {
                 layers: [lightmap, carAccidents]
             });
         
-        // Heatmap layer for map
-            $.getJSON(newtry, function(response){
-                console.log(response);
-                var heatArray = [];
+        // // Heatmap layer for map
+        //     $.getJSON(newtry, function(response){
+        //         console.log(response);
+        //         var heatArray = [];
             
-                for (var i = 0; i<response.length; i++){
-                    var location = response[i].location;
+        //         for (var i = 0; i<response.length; i++){
+        //             var location = response[i].location;
             
-                    if(location){
-                        heatArray.push([location.coordinates[1], location.coordinates[0]]);
-                    }
-                }
+        //             if(location){
+        //                 heatArray.push([location.coordinates[1], location.coordinates[0]]);
+        //             }
+        //         }
             
-                var heat = L.heatLayer(heatArray, {
-                    radius: 20,
-                    blur: 35
-                }).addTo(myMap);
-            });
+        //         var heat = L.heatLayer(heatArray, {
+        //             radius: 20,
+        //             blur: 35
+        //         }).addTo(myMap);
+        //     });
 
-            L.control.layers(baseMaps, overlayMaps, heat, {
-                collapsed: false
-            }).addTo(map);
+        //     L.control.layers(baseMaps, overlayMaps, heat, {
+        //         collapsed: false
+        //     }).addTo(map);
 
         // Graph of weather distribution
 
