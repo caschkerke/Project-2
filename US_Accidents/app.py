f1039b439dfed3319a769e4e9c4337be891de9bf
caschkerke <caschkerke@gmail.com>
2020-06-14 20:52:41 -0500

    final upload

--- US_Accidents/app.py
+++ US_Accidents/app.py
@@ -14,7 +14,10 @@ from flask import (
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect, func, create_engine, MetaData, Column, Text, Integer, Date, TIMESTAMP, REAL, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.engine import reflection
+# from flask_marshmallow import Marshmallow
+# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 
 #################################################
 # Flask Setup
@@ -31,73 +34,148 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "p
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db functions as engine with app configuration as is
 db = SQLAlchemy(app)
+meta = db.metadata
+engine = db.engine
+# session = scoped_session(sessionmaker(bind=engine))
+insp = reflection.Inspector.from_engine(engine)
 
-class db2019(db.Model):
-
+# Models
+db_2019_table = Table('db_2019', meta, autoload=True, autoload_with=engine)
+class db_2019(db.Model):
     __tablename__ = 'db_2019'
 
-    ID = db.Column(db.Text, primary_key=True)
-    severity = db.Column(db.Integer)
-    start_time = db.Column(db.TIMESTAMP)
-    end_time = db.Column(db.Text)
-    start_lat = db.Column(db.REAL)
-    start_lng = db.Column(db.REAL)
-    city = db.Column(db.Text)
-    state = db.Column(db.Text)
-    zipcode = db.Column(db.Text)
-    weather_timestamp = db.Column(db.Text)
-    temperature = db.Column(db.REAL)
-    wind_chill = db.Column(db.REAL)
-    humidity = db.Column(db.REAL)
-    pressure = db.Column(db.REAL)
-    visibility = db.Column(db.REAL)
-    wind_direction = db.Column(db.Text)
-    wind_speed = db.Column(db.REAL)
-    precipitation = db.Column(db.REAL)
-    weather_condition = db.Column(db.Text)
-    Bump = db.Column(db.Integer)
-    Crossing = db.Column(db.Integer)
-    Give_Way = db.Column(db.Integer)
-    Junction = db.Column(db.Integer)
-    No_Exit = db.Column(db.Integer)
-    Railway = db.Column(db.Integer)
-    Roundabout = db.Column(db.Integer)
-    Station = db.Column(db.Integer)
-    Stop = db.Column(db.Integer)
-    Traffic_Calming = db.Column(db.Integer)
-    Traffic_Signal = db.Column(db.Integer)
-    Turning_Loop = db.Column(db.Integer)
+distinct_weather_table = Table('distinct_weather', meta, autoload=True, autoload_with=engine)
+class distinct_weather(db.Model):
+    __tablename__ = 'distinct_weather'
+
+# # Marshmallow Schemas
+# class db_2019Schema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = db_2019
+#         include_relationships = True
+#         load_instance = True
+
+# class distinct_weatherSchema(SQLAlchemyAutoSchema):
+#     class Meta:
+#         model = distinct_weather
+#         include_relationships = True
+#         load_instance = True
 
 # Creating route that renders index.html template
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
-    # POST means we've got a postback requesting filtered data.
-    if request.method == 'POST':
-        # Handle the filtering of data as needed and bind.
-        # Data that is posted back is held in the request object
-        # request.form['fieldname']
-
-        logging.info("POSTed!")
-        q = db2019.query.filter(db2019.weather_condition == 'Fair')
-
-        return render_template("index.html", q=q)
-    else:
-        q = db2019.query.filter(db2019.weather_condition == 'Fair')
-        for row in q:
-            pp.pprint(row.__dict__)
+    
         return render_template('index.html')
 
+# --- query examples ----
+# distinct_weather.query.with_entities(distinct_weather.weather_condition).all()
+# distinct_weather.query
+# db2019.query.filter(db2019.weather_condition == 'Fair')
+
 # Creating route for the data calls
-# @app.route("/data", methods=['POST', 'GET'])
-# def data():
+@app.route("/dropdown", methods=['GET'])
+def dropdown():
 
-    # q = session.query(db2019).filter(db2019.weather_condition == 'Fair')
-    # # q = db2019.query(db2019.Weather_Condition).first()
-    # qprint = print(q)
+    if request.method == 'GET':
+        weaList = distinct_weather.query.with_entities(distinct_weather.weather_condition).all()
 
-    # return qprint
+    return jsonify(weaList)
+
+@app.route("/data", methods=['POST'])
+def data():
+
+# POST means we've got a postback requesting filtered data.
+    if request.method == 'POST':
+        response = db_2019.query.with_entities(db_2019.start_lat, \
+        db_2019.start_lng, \
+        db_2019.city, \
+        db_2019.zipcode, \
+        db_2019.visibility, \
+        db_2019.weather_condition, \
+        db_2019.temperature, \
+        db_2019.severity, \
+        db_2019.start_time).limit(10000).all()
+
+    return jsonify(response)
+
+        # Handle the filtering of data as needed and bind.
+        # Data that is posted back is held in the request object
+        # weather = request.form.get('weather')
+        # env = request.form.get("env")
+
+        # weaList = distinct_weather.query.with_entities(distinct_weather.weather_condition).all()
+        # newList = []
+        # for item in weaList:
+        #     newList.append(item)
+        # weaList = [item.replace("'", "") for item in weaList]
+        # newList = [item.replace("'", "") for item in newList]
+        # print(newList)
+        # weaList = weaList.to_dict()
+        # for item in newList:
+        #     if item == (f"('{weather}',)"):
+        #         print(item)
+        # logging.info(weather)
+        # if weather == "Cloudy":
+        #     print("true")
+
+    # return jsonify(newList)
+    #     # envOption = request.get_json(env)
+    #     logging.info("POSTed!")
+
+    #     # for row in request:
+    #     #     pp.pprint(row.__dict__)
+
+    #     # model_list = [bump, crossing, give_way, junction, no_exit, railway, roundabout, station, stop, traffic_calming, traffic_signal, turning_loop]
+
+    #     # for model in model_list:
+    #     #     if model == request[env]:
+    #     #         envQ = model.query
+    #     # weaList = distinct_weather.query.with_entities(distinct_weather.weather_condition).all()
+    #     weaQ = db_2019.query
+    #     weaFilt = weaQ.filter_by(weather_condition={weather})
+    #     # jsonWea = json.dumps(weaList)
+    #     # print 
+    #     # for object in jsonWea:
+    #     #     print(object)
+    #     #     if item == weather:
+    #     #         weaQ = db_2019.query.with_entities(db_2019.weather_condition == item).all()
+    
+    # # # for row in weaList:
+    # # #     pp.pprint(row.__dict__)
+    # # #     weaList = pp.pprint(weaList)
+
+    # # return jsonify(envQ, weaQ)
+    # # return json.dumps([row.__dict__ for row in weaQ])
+    # # return json.dumps([row.__dict__ for row in query])
+    # return jsonify(weaFilt.all().as_dict())
+    #     weaC = db_2019(weather_condition="Fair")
+    #     weaC_schema = db_2019Schema()
+    #     dump_data = weaC_schema(weaC)
+    #     session.add(weaC)
+    #     session.commit()
+
+    #     print(dump_data)
+    # return dump_data
+
+    
+    # -- Out of ideas --
+    # can't run list comprehension on result object, tried to convert result to new list, still registers as the response rather than actual list
+    # can't run query filter against uncomprehended list elements due to undesired characters
+    # json.dumps() returns list by character rather than by item, indicating that each item isn't being properly recognized?
+    # jsonify returns proper list of variables
+    # unable to set list as jsonified version of itself, leaving me unable to create parseable format
+    # psycopg2 can't set values variably, meaning I need to set the variable before I query
+    # cannot iterate through response objects
+    # iterating through list returns last value in list, unable to apply formatting to conditional in order to return appropriate value regardless
+    # attempted to incorporate marshmallow as a means to parse, schema object not callable
+    # only option is to hardcode each of the 101 weather variables and run matches against those?
+    # would need to then code coditionals for when both the weather variables and the environmental variable match
+    # when checking for presence of environmental selection, would need to add check for weather condition selection, and then rerun weather conditionals
+    # could create a function to run the weather conditional check and apply from here, but would still result in about 200 lines of code
+    # SQLAlchemy has no native ability to jsonify returned list, and I am unable to run parsing against any response objects generated.
+    # can't think of efficient way to create variable marker generation as theorhetically working methods would generate massive loadtimes..
+
         
 
 if __name__ == "__main__":
--- US_Accidents/static/js/app.js
+++ US_Accidents/static/js/app.js
@@ -2,14 +2,11 @@
 // Took the opportunity to encase all functions within for the sake of functionality and simplicity.
 $(document).ready(function() {   
 
-// Selecting dropdown menus.
+    // Selecting dropdown menus.
     var weatherMenu = $("#selWeather");
     var envMenu = $("#selEnvironment");
 
-// Defining placeholder response
-    var response = ""
-
-// Due to the nature of the visualizations relating to the environments, and the fact that the different environments are individual columns, using a static list.
+    // Due to the nature of the visualizations relating to the environments, and the fact that the different environments are individual columns, using a static list.
 
     envList = ["Bump",
                 "Crossing",
@@ -23,153 +20,79 @@ $(document).ready(function() {
                 "Traffic_Calming",
                 "Traffic_Signal",
                 "Turning_Loop"]
+
     for (var i = 0; i < envList.length; i++){
         var o = new Option(envList[i], envList[i]);
         $(o).html(envList[i]);
         envMenu.append(o);
     }
+
+    var weaList = []
     
-    weaList = ['Fair',
-        'Mostly Cloudy',
-        'Fog',
-        'Partly Cloudy',
-        'Cloudy',
-        'Partly Cloudy / Windy',
-        'Light Rain',
-        'Wintry Mix',
-        'Light Snow',
-        'Rain',
-        'Haze',
-        'Snow',
-        'Heavy Rain',
-        'Mostly Cloudy / Windy',
-        'Patches of Fog',
-        'Heavy Snow',
-        'Heavy Rain / Windy',
-        'Light Drizzle',
-        'Light Freezing Rain',
-        'Mist',
-        'Light Rain / Windy',
-        'Showers in the Vicinity',
-        'Drizzle',
-        'T-Storm',
-        'Shallow Fog',
-        'Cloudy / Windy',
-        'Light Rain Shower',
-        'N/A Precipitation',
-        'Freezing Rain',
-        'Light Rain with Thunder',
-        'Rain / Windy',
-        'Thunder',
-        'Light Snow / Windy',
-        'Snow / Windy',
-        'Heavy Drizzle',
-        'Fair / Windy',
-        'Snow and Sleet',
-        'Sleet',
-        'Heavy T-Storm',
-        'Light Drizzle / Windy',
-        'Light Freezing Rain / Windy',
-        'Light Sleet',
-        'Heavy Sleet',
-        'Wintry Mix / Windy',
-        'Light Snow Shower',
-        'Smoke',
-        'Drizzle and Fog',
-        'T-Storm / Windy',
-        'Funnel Cloud',
-        'Thunder in the Vicinity',
-        'Heavy Snow / Windy',
-        'Light Freezing Drizzle',
-        'Fog / Windy',
-        'Light Snow and Sleet',
-        'Light Snow Grains',
-        'Thunder / Windy',
-        'Haze / Windy',
-        'Sand / Dust Whirlwinds',
-        'Heavy T-Storm / Windy',
-        'Blowing Snow',
-        'Blowing Dust',
-        'Light Rain Shower / Windy',
-        'Small Hail',
-        'Squalls / Windy',
-        'Blowing Dust / Windy',
-        'Drizzle / Windy',
-        'Partial Fog / Windy',
-        'Light Snow with Thunder',
-        'Blowing Snow / Windy',
-        'Partial Fog',
-        'Rain Shower',
-        'Widespread Dust / Windy',
-        'Sand / Dust Whirlwinds / Windy',
-        'Tornado',
-        'Snow and Thunder',
-        'Light Snow and Sleet / Windy',
-        'Snow and Sleet / Windy',
-        'Heavy Snow with Thunder',
-        'Thunder / Wintry Mix / Windy',
-        'Clear',
-        'Overcast',
-        'Light Ice Pellets',
-        'Scattered Clouds',
-        'Light Thunderstorms and Rain',
-        'Heavy Thunderstorms and Rain',
-        'Thunderstorms and Rain',
-        'Light Freezing Fog',
-        'Rain Showers',
-        'Thunderstorm',
-        'Light Thunderstorms and Snow',
-        'Widespread Dust',
-        'Light Snow Showers',
-        'Heavy Blowing Snow',
-        'Squalls',
-        'Light Rain Showers',
-        'Ice Pellets',
-        'Low Drifting Snow',
-        'Smoke / Windy',
-        'Hail',
-        'Heavy Freezing Drizzle',
-        'Thunder and Hail / Windy']
-
-    for (var i = 0; i < weaList.length; i++){
-        var o = new Option(weaList[i], weaList[i]);
-        $(o).html(weaList[i]);
-        weatherMenu.append(o);
-    }
-
-    $.get('/data', function(response) {
-            console.log(response);
-        }
-    );
-
-// Function to create map with unfiltered 2019 data.
-    
-
-        
-    
-// Function to create markers based on the response returned from Flask
-    
-
-// menuChange function will take the place of updatePlotly. 
-// When any selection fields register a change, this function will trigger and attempt to update all visualizations based on current selection.
-    var menuChange = function(){
-        
-    // defining the values of each dropdown menu as variables.
-        var weather = $('#selWeather').value;
-        var env = $('#selEnvironment').value;
-
-    // calling on Flask for selected weather and environment variables
-        $.post('/data',
-            {
-                weather: weather,
-                env: env,
-            },
-            function(response) {
-                console.log(response);
+    // Populating dropdown list with weather options
+    $.ajax({
+        url: '/dropdown',
+        type: 'GET',
+        dataType: 'json',
+        success: function(res){
+            for (var i = 0; i < res.length; i++){
+                var o = new Option(res[i], res[i]);
+                $(o).html(res[i]);
+                weatherMenu.append(o)
+                weaList.push(o);
             }
-        );
+        }
+    });
 
-    createMarkers(response);
+    // Initializing blank map on page load.
+    var map = L.map('countryMap').setView([38.51073, -96.4247], 5);
+    L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "streets-v11",
+        accessToken: API_KEY
+        }).addTo(map);
+
+    // menuChange function will take the place of updatePlotly. 
+    // When any selection fields register a change, this function will trigger and attempt to update all visualizations based on current selection.
+    var menuChange = function(){
+
+        var weather = $('#selWeather').val();
+        var env = $('#selEnvironment').val();
+        console.log(weather, env)
+        
+        // map.removeLayer(accidentLocations)
+
+        // Function to create markers based on the response returned from Flask
+        function createMarkers(accidentLocations) {
+            
+            var accidentLocations = [];
+
+            // // defining the values of each dropdown menu as variables.
+            // var weather = $('#selWeather').val();
+            // var env = $('#selEnvironment').val();
+
+            // calling on Flask for selected weather and environment variables
+            $.post('/data', {'weather': weather, 'env': env}, function(response){
+                    console.log(response)
+                    for (var i = 0; i < response.length; i++){
+        
+                        var accidentMarker = L.marker([response[i][0], response[i][1]])
+                            .bindPopup("<h3>Weather: " + response[i][5] + 
+                            "<h3><h3>City: " + response[i][2] + 
+                            "<h3><h3>Zipcode: " + response[i][3] + 
+                            "<h3><h3>Visibility (mi): " + response[i][4] + 
+                            "<h3><h3>Temperature (F): " + response[i][6] + 
+                            "<h3><h3>Severity: " + response[i][7] + 
+                            "<h3><h3>Time of Accident: " + response[i][8] + "</h3>")
+                
+                        accidentLocations.push(accidentMarker);
+                    }
+
+                    L.layerGroup(accidentLocations).addTo(map)
+                }, "json");
+        }
+        createMarkers();
 
     // ---- Visualizations based on user selection begin here ---- Still needs work ----
     // these will need to be coded to be dynamic using the outputs generated from the conditionals above.
@@ -201,79 +124,32 @@ $(document).ready(function() {
 
         // Graph of road environment distribution
 
+        // pie chart for weather distribution
+        // var chart = new CanvasJS.Chart("chartContainer", {
+        //     animationEnabled: true,
+        //     title: {
+        //         text: "Desktop Search Engine Market Share - 2016"
+        //     },
+        //     data: [{
+        //         type: "pie",
+        //         startAngle: 240,
+        //         yValueFormatString: "##0.00\"%\"",
+        //         indexLabel: "{label} {y}",
+        //         dataPoints: [
+        //             {y: 79.45, label: "Google"},
+        //             {y: 7.31, label: "Bing"},
+        //             {y: 7.06, label: "Baidu"},
+        //             {y: 4.91, label: "Yahoo"},
+        //             {y: 1.26, label: "Others"}
+        //         ]
+        //     }]
+        // });
+        // chart.render();
+
+        // Chart for environment distribution
+
     }
 
-    // pie chart for weather distribution
-    // var chart = new CanvasJS.Chart("chartContainer", {
-    //     animationEnabled: true,
-    //     title: {
-    //         text: "Desktop Search Engine Market Share - 2016"
-    //     },
-    //     data: [{
-    //         type: "pie",
-    //         startAngle: 240,
-    //         yValueFormatString: "##0.00\"%\"",
-    //         indexLabel: "{label} {y}",
-    //         dataPoints: [
-    //             {y: 79.45, label: "Google"},
-    //             {y: 7.31, label: "Bing"},
-    //             {y: 7.06, label: "Baidu"},
-    //             {y: 4.91, label: "Yahoo"},
-    //             {y: 1.26, label: "Others"}
-    //         ]
-    //     }]
-    // });
-    // chart.render();
-
-    // Chart for environment distribution
-
-// Event listeners that will trigger the menuChange function on any selection change.
-    $('#selWeather').change(menuChange);
-    $('#selEnvironment').change(menuChange);
+    // Event listener that will trigger the menuChange function on button click.
+    $("button").click(menuChange);
 });
-
-function createMarkers(response) {
-            
-    var accidentLocations = [];
-
-    for (var index=0; index < response.length; index++) {
-
-        var accidentMarker = L.marker([response.start_lat, response.start_lng])
-            .bindPopup("<h3>Weather: " + response.Weather_Condition + 
-            "<h3><h3>Severity: " + response.Severity + 
-            "<h3><h3>Time of Accident: " + response.Start_Time + "</h3>")
-
-        accidentLocations.push(accidentMarker);
-    }
-    // if (typeof myMap != "undefined") {
-    //     myMap.remove()
-    //     $("#countryMap").html("")}
-
-    myMap = createMap(accidentLocations);
-    L.layerGroup(accidentLocations).addTo(myMap)
-
-}
-
-function createMap(accidentLocations) {
-
-    var lightMap = L.map('countryMap').setView([48.4284, -123.3656], 2);
-    
-    L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 10,
-        id: "streets-v11",
-        accessToken: API_KEY
-        });
-
-        var baseMaps = {
-            "Light Map": lightMap
-        };
-
-        var overlayMaps = {
-            "Car Accidents": accidentLocations
-        };
-
-        L.control.layers(baseMaps, overlayMaps, {
-            collapsed: false
-        }).addTo(lightMap);
-}
